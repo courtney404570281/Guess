3b67c81f8d22e24e2c9229c07ddf30614b56ae54
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-11 21:57:37 +0800

    Guess Number

--- app/src/main/java/com/courtney/guess/MainActivity.kt
+++ app/src/main/java/com/courtney/guess/MainActivity.kt
@@ -3,6 +3,7 @@ package com.courtney.guess
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,11 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: ${secretNumber.secret}")
     }
 
     fun check(view : View) {
         val n = ed_number.text.toString().toInt()
         println("n: $n")
         Log.d("MainActivity", "number: $n")
+
+        val diff = secretNumber.validate(n)
+
+        var message = "Bingo!"
+        when {
+            diff < 0 -> message = "Higher"
+            diff > 0 -> message = "Lower"
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
+
     }
 }
